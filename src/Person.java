867f66c87c88a1048bebd28031388aebd5708631
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2023-02-06 22:49:40 +0000

    person class added

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+    String surname;
+    String email;
+
+    public Person(String newName, String newSurname, String newEmail) {
+        name = newName;
+        surname = newSurname;
+        email = newEmail;
+    }
+
+}
--- src/Theatre.java
+++ src/Theatre.java
@@ -1,11 +1,10 @@
-import java.lang.reflect.Member;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Theatre {
     static ArrayList<int[]> rows = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         rows.add(new int[12]);
         rows.add(new int[16]);
@@ -15,44 +14,51 @@ public class Theatre {
     }
     public static void mainMenu() {
         Scanner in = new Scanner(System.in);
-        boolean flag = true;
-        while (flag) {
-            printMenu();
-            System.out.print("=> ");
-            String menuChoice = in.nextLine();
-            switch (menuChoice) {
-                case "0":
-                    flag = false;
-                    System.out.println("Quitting...");
-                    break;
-                case "1":
-                    buyTicket();
-                    break;
-                case "2":
-                    printSeatingArea();
-                    break;
-                case "3":
-                    System.out.println("3");
-                    break;
-                case "4":
-                    System.out.println("4");
-                    break;
-                case "5":
-                    System.out.println("5");
-                    break;
-                case "6":
-                    System.out.println("6");
-                    break;
-                case "7":
-                    System.out.println("7");
-                    break;
-                case "8":
-                    System.out.println("8");
-                    break;
-                default:
-                    System.out.println("Invalid choice, please select again");
+        String menuChoice = "";
+        printMenu();
+        System.out.print("=> ");
+        menuChoice = in.nextLine();
+        switch (menuChoice) {
+            case "0":
+                System.out.println("Quitting...");
+                System.exit(0);
+                break;
+            case "1":
+                buyTicket();
+                mainMenu();
+                break;
+            case "2":
+                printSeatingArea();
+                mainMenu();
+                break;
+            case "3":
+                cancelTicket();
+                mainMenu();
+                break;
+            case "4":
+                showAvailable();
+                mainMenu();
+                break;
+            case "5":
+                System.out.println("5");
+                mainMenu();
+                break;
+            case "6":
+                System.out.println("6");
+                mainMenu();
+                break;
+            case "7":
+                System.out.println("7");
+                mainMenu();
+                break;
+            case "8":
+                System.out.println("8");
+                mainMenu();
+                break;
+            default:
+                System.out.println("Invalid choice, please select again");
+                mainMenu();
 
-            }
         }
     }
 
@@ -70,33 +76,28 @@ public class Theatre {
     }
 
     public static void buyTicket() {
-        boolean flag = true;
-        Scanner in = new Scanner(System.in);
         int rowNum = 0;
         int seatNum = 0;
-        while (flag) {
             System.out.print("Enter desired row number: ");
-            rowNum = in.nextInt();
+            rowNum = input.nextInt();
             if (rowNum > 3 || rowNum < 1) {
                 System.out.println("desired row:" + rowNum + " do not exist");
                 System.out.println("Please try again");
-                continue;
+                mainMenu();
             }
             int rowSize = getRowSize(rowNum);
             System.out.print("Enter desired seat number: ");
-            seatNum = in.nextInt();
+            seatNum = input.nextInt();
             if (seatNum > rowSize || seatNum < 1) {
                 System.out.println("desired seat:" + seatNum + " of row:"+ rowNum + " do not exist");
                 System.out.println("Please try again");
-                continue;
+                mainMenu();
             }
             if (rows.get(rowNum - 1)[seatNum - 1] != 0) {
                 System.out.println("desired seat:" + seatNum + " of row:"+ rowNum + " is already occupied");
                 System.out.println("Please try again");
-                continue;
+                mainMenu();
             }
-            flag = false;
-        }
         rows.get(rowNum - 1)[seatNum - 1] = 1;
     }
 
@@ -131,5 +132,42 @@ public class Theatre {
             System.out.println();
         }
 
+    }
+
+    public static void cancelTicket() {
+        int rowNum = 0;
+        int seatNum = 0;
+        System.out.print("Enter desired row number: ");
+        rowNum = input.nextInt();
+        if (rowNum > 3 || rowNum < 1) {
+            System.out.println("desired row:" + rowNum + " do not exist");
+            System.out.println("Please try again");
+            mainMenu();
+        }
+        int rowSize = getRowSize(rowNum);
+        System.out.print("Enter desired seat number you want to cancel: ");
+        seatNum = input.nextInt();
+        if (seatNum > rowSize || seatNum < 1) {
+            System.out.println("desired seat:" + seatNum + " of row:"+ rowNum + " do not exist");
+            System.out.println("Please try again");
+            mainMenu();
+        }
+        if (rows.get(rowNum - 1)[seatNum - 1] == 0) {
+            System.out.println("desired seat:" + seatNum + " of row:"+ rowNum + " is not occupied");
+            System.out.println("Please try again");
+            mainMenu();
+        }
+        rows.get(rowNum - 1)[seatNum - 1] = 0;
+    }
+    public static void showAvailable() {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Seats available in row " + (i+1) + ": ");
+            for (int j = 0; j < rows.get(i).length; j++) {
+                if (rows.get(i)[j] == 0) {
+                    System.out.print((j+1) + ", ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
