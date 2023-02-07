ad9e07b792f80380ee0456463c431fd8da17c69f
Spicyhuen <hyuen.yutico@gmail.com>
2023-02-07 15:43:42 +0000

    added ticket sorting

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Collections;
 
 public class Theatre {
+    static ArrayList<Ticket> ticketList = new ArrayList<Ticket>();
     static ArrayList<int[]> rows = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
@@ -49,11 +52,11 @@ public class Theatre {
                 mainMenu();
                 break;
             case "7":
-                System.out.println("7");
+                showTicketInfo();
                 mainMenu();
                 break;
             case "8":
-                System.out.println("8");
+                sortTickets();
                 mainMenu();
                 break;
             default:
@@ -99,7 +102,24 @@ public class Theatre {
                 System.out.println("Please try again");
                 mainMenu();
             }
+        String[] personInfo = getPersonInfo();
+        Person person = new Person(personInfo[0], personInfo[1], personInfo[2]);
+        System.out.print("Enter ticket price: ");
+        float price = input.nextFloat();
+        Ticket ticket = new Ticket(rowNum,seatNum,price, person);
+        ticketList.add(ticket);
         rows.get(rowNum - 1)[seatNum - 1] = 1;
+    }
+
+    public static String[] getPersonInfo() {
+        String[] result = new String[3];
+        System.out.print("Enter your name: ");
+        result[0] = input.next();
+        System.out.print("Enter your surname: ");
+        result[1] = input.next();
+        System.out.print("Enter your email: ");
+        result[2] = input.next();
+        return result;
     }
 
     public static int getRowSize(int rowNum) {
@@ -111,7 +131,7 @@ public class Theatre {
     }
 
     public static void printSeatingArea() {
-        System.out.println("    *************");
+        System.out.println("\n    *************");
         System.out.println("    *   STAGE   *");
         System.out.println("    *************");
         String space = " ";
@@ -159,6 +179,14 @@ public class Theatre {
             mainMenu();
         }
         rows.get(rowNum - 1)[seatNum - 1] = 0;
+        // loops through ticketList and check the desired ticket to be cancelled
+        for (int i = 0; i < ticketList.size(); i++) {
+            if (ticketList.get(i).seat == seatNum && ticketList.get(i).row == rowNum) {
+                ticketList.remove(i);
+            }
+        }
+
+
     }
     public static void showAvailable() {
         for (int row = 0; row < 3; row++) {
@@ -172,4 +200,54 @@ public class Theatre {
             System.out.println();
         }
     }
+
+    public static void showTicketInfo() {
+        float totalPrice = 0f;
+        for (Ticket ticket : ticketList) {
+            totalPrice+= ticket.price;
+            ticket.printDetails();
+            System.out.println();
+        }
+        if (totalPrice > 0) {
+            System.out.println("Total price: " + totalPrice);
+        }
+    }
+
+    public static void sortTickets() {
+        for (int i = 0; i < ticketList.size(); i++) {
+            for (int j = i; j < ticketList.size(); j++) {
+                if (ticketList.get(j).price < ticketList.get(i).price) {
+                    swapTicketInfo(ticketList.get(j), ticketList.get(i));
+                }
+            }
+        }
+    }
+
+    public static void swapTicketInfo(Ticket ticket1, Ticket ticket2) {
+        float tempPrice = ticket1.price;
+        ticket1.price = ticket2.price;
+        ticket2.price = tempPrice;
+
+        int tempRow = ticket1.row;
+        ticket1.row = ticket2.row;
+        ticket2.row = tempRow;
+
+        int tempSeat = ticket1.seat;
+        ticket1.seat = ticket2.seat;
+        ticket2.seat = tempSeat;
+
+
+        String tempName = ticket1.person.name;
+        ticket1.person.name = ticket2.person.name;
+        ticket2.person.name = tempName;
+
+        String tempSurname = ticket1.person.surname;
+        ticket1.person.surname = ticket2.person.surname;
+        ticket2.person.surname = tempSurname;
+
+        String tempEmail = ticket1.person.email;
+        ticket1.person.email = ticket2.person.email;
+        ticket2.person.email = tempEmail;
+    }
+
 }
