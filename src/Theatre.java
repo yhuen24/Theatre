5e691740073ce251cdbb494bf7cd528d7545b420
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2023-02-27 11:54:59 +0000

    Removed unused imports

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,10 +1,8 @@
-import javax.security.auth.kerberos.KerberosTicket;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
